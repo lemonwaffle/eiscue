1a83cc82674a13cc74291c9c8575b528f3321c4c
lemonwaffle <ang.yizhe@u.nus.edu>
2020-06-10 11:58:08 +0700

    :D

--- train_net.sh
+++ train_net.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/retinanet_R_50_FPN.yaml --exp-name retinanet_small
+# CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/retinanet_R_50_FPN.yaml --exp-name retinanet_small
 
-# CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/faster_rcnn_R_50_FPN.yaml --exp-name rcnn_bl
+CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/faster_rcnn_R_50_FPN.yaml --exp-name rcnn_bl
