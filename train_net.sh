1d675e84629ae366b082cacb99a0e927fd5e029d
Ang Yi Zhe <angyizhe@MacBook-Pro.local>
2020-06-11 10:50:22 +0800

    :D

--- train_net.sh
+++ train_net.sh
@@ -2,4 +2,6 @@
 
 CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/retinanet_R_50_FPN.yaml --exp-name retinanet_freeze
 
-# CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/faster_rcnn_R_50_FPN.yaml --exp-name rcnn_bl --dist-url auto
+CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/cascade_mask_rcnn.yaml --exp-name cascade_bl
+
+CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file configs/faster_rcnn_R_50_FPN.yaml --exp-name rcnn_bl
